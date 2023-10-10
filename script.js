eaa715805541084c2972ca8e85719d9cd97efc07
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-10-10 10:07:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+
+const button = document.getElementById("btn");
+const container = document.getElementById("weatherData");
+
+button.addEventListener("click",() => {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de')
+         .then(response => response.json())
+	.then(data => {
+         container.innerHTML += `<div>Current weather in London: ${data.weather[0].main}</div>`;;
+         })
+
+	//container.appendChild(div);
+})
